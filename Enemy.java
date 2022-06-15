2bff7f870b7d81fd43e3c7e40fa50395d58d3ccc
Daniel Santiago Pérez D <68489860+mr-zero-03@users.noreply.github.com>
2022-06-14 23:26:30 -0500

    Create Enemy.java

---
+++ Enemy.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class Enemy {
+
+  int lives = 0;
+  int damage = 0;
+  int skinId = 0;
+
+  public Enemy( int lives, int damage, int skinId ) {
+    this.lives = lives;
+    this.damage = damage;
+    this.skinId = skinId;
+  }
+
+  //Lives
+  public void die() {
+    
+  }
+
+  public void loseLive() {
+    this.lives -= 1;
+
+    if ( this.lives == -1 ) {
+      die();
+    }
+  }
+
+  //Movement
+  public void move() {
+  }
+
+  public void attack() {
+    System.out.println( "Attacking with Sword" );
+  }
+
+}
